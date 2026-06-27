484c574b2cf06bcd68ce558965027652fb2405d2
chasebassett09-star <chasebassett09@gmail.com>
2026-06-27 11:20:56 -0600

    Update Config.h

--- Lightsaber/Config.h
+++ Lightsaber/Config.h
@@ -7,6 +7,11 @@
 #define SDA_PIN 21
 #define SCL_PIN 22
 #define NUM_LEDS 144
+#define BATTERY_PIN 34
+#define BATTERY_MAX 12.6
+#define BATTERY_MIN 9.0
+#define R1 100000.0
+#define R2 33000.0
 const uint8_t ignitionSpeed = 2;
 const uint16_t frameTime = 10;
 const char* WIFI_NAME="Lightsaber";
