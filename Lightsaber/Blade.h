955b08f96f42db6517f7948549d787b1f3e616c4
chasebassett09-star <chasebassett09@gmail.com>
2026-06-27 12:13:03 -0600

    Update Blade.h

--- Lightsaber/Blade.h
+++ Lightsaber/Blade.h
@@ -2,14 +2,6 @@
 #define BLADE_H
 #include <Arduino.h>
 #include "BladeTypes.h"
-enum BladeState{BLADE_OFF,BLADE_IGNITING,BLADE_ON,BLADE_RETRACTING};
-enum BladeMode{MODE_STABLE,MODE_UNSTABLE,MODE_RAINBOW};
-struct BladeSettings{
- uint8_t r,g,b;
- uint8_t brightness;
- BladeMode mode;
- uint16_t litPixels;
-};
 extern BladeSettings blade;
 void bladeBegin();
 void bladeUpdate();
