fe4684fc387e20d93674d498f4afafb905ae8ff8
chasebassett09-star <chasebassett09@gmail.com>
2026-06-27 11:52:07 -0600

    Update Hardware.h

--- Lightsaber/Hardware.h
+++ Lightsaber/Hardware.h
@@ -1,5 +1,16 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
+
 void initHardware();
 void updateHardware();
+
+// Individual initialization functions
+void initLED();
+void initButton();
+void initI2C();
+void initBNO();
+void initDFPlayer();
+void initBluetooth();
+void initWiFi();
+
 #endif
